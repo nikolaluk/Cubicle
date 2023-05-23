965ae93d856f3c3b33691d6adaf64e20928c74cd
CEO OF STOICISM <nikola.lukov.2006@gmail.com>
2023-05-23 22:40:57 +0300

    Implemented cube adding POST request handling

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
 
+//GET
 router.get('/', (req,res) => {
     res.render("index");
 });
@@ -10,6 +13,23 @@ router.get('/create', (req,res) => {
 
 router.get('/about',(req,res) => {
     res.render('about');
-})
+});
+
+//POST
+router.post('/create',(req,res) => {
+    const {name,description,imageUrl,difficultyLevel} = req.body;
+    let id = new Date().getTime();
+    
+    fs.readFile(path.resolve(__dirname,'../data/cubes.json'),(err,data) => {
+        
+        let cubes = JSON.parse(data);
+        cubes.push({name,description,imageUrl,difficultyLevel,id});
+        let json = JSON.stringify(cubes);
+
+        fs.writeFile(path.resolve(__dirname,'../data/cubes.json'),json, () => console.log("Cube added"));
+    });
+
+    res.redirect('/');
+});
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -17,8 +17,6 @@ handlebarsConfig(app);
 //Routes
 app.use(homeController);
 
-app.get('/create', (req,res) => {
-    res.render('create');
-});
+
 
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
