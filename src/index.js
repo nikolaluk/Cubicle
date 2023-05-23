97ecf6858165aca884f91a0b152f36151c57c2df
CEO OF STOICISM <nikola.lukov.2006@gmail.com>
2023-05-23 20:30:05 +0300

    Handlebars set-up and homepage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
 const port = 5000;
 
+//Handlebars config
+app.engine('hbs',handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+
+//Routes
 app.get('/', (req,res) => { 
-    res.send("Hello world");
+    res.render("index");
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
