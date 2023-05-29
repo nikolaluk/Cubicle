99482eba76f75f5d97aade185654288f2feccc0f
CEO OF STOICISM <nikola.lukov.2006@gmail.com>
2023-05-29 21:46:54 +0300

    DB config

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 //System modules
 const express = require('express');
-const path = require('path');
+const mongoose = require('mongoose');
 
 //Modules
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
+const dbConfig = require('./config/dbConfig');
 const homeController = require('./controllers/homeController');
 
+//App
 const app = express();
 const port = 5000;
 
 //Config
+dbConfig();
 expressConfig(app);
 handlebarsConfig(app);
 
 //Routes
 app.use(homeController);
-
-//404
-
 
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
